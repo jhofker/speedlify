6f13dae44edc98ec68d9d947f399f1bda3ad5c1e
Jordan Hofker <jordan.hofker@hudl.com>
2020-07-01 15:22:05 -0500

    Update sites.js

--- _data/sites.js
+++ _data/sites.js
@@ -1,36 +1,28 @@
 module.exports = {
-	netlify: {
-		description: "Netlify web sites",
+	personal: {
+		description: "Personal",
 		options: {
 			freshChrome: "site",
 		},
 		urls: [
-			"https://www.netlify.com/",
-			"https://www.netlify.com/donation-matching/",
-			"https://www.netlify.com/products/build/plugins/",
-			"https://www.netlify.com/with/sitecore/",
-			"https://www.netlify.com/with/drupal/",
-			"https://www.netlify.com/with/wordpress/",
-			"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
-			"https://jamstackconf.com/",
-			"https://jamstackconf.com/virtual/",
+			"https://sidelight.kerjo.com/",
+			"https://www.kerjo.com/",
+			"https://jordan.hofker.org/"
 		]
 	},
-	ssg: {
-		description: "Static Site Generator web sites",
+	client: {
+		description: "Client",
 		options: {
 			// if the sites don’t share assets on the same domain, we can reset
 			// chrome with each run instead of each site in every run (it’s faster)
 			freshChrome: "run"
 		},
 		urls: [
-			"https://www.11ty.dev/",
-			"https://www.gatsbyjs.org/",
-			"https://gohugo.io/",
-			"https://nextjs.org/",
-			"https://nuxtjs.org/",
-			// "https://vuepress.vuejs.org/", // kept timing out in lighthouse
-			"https://gridsome.org/",
+			"https://www.camerashq.com/",
+			"https://www.greatharvestbreadomaha.com/",
+			"https://www.hofkerautomotive.com/",
+			"https://www.mollymarketing.com/",
+			"https://www.wenninghoff.com/"
 		]
 	}
 };
